c1945625ba5ce4ba9177a31d8dfa761f29b14d89
Malcolm-XY <m18307300600@gmail.com>
2025-07-21 21:23:59 +0900

    20250721

--- cnn_subnetworks_val_circle.py
+++ cnn_subnetworks_val_circle.py
@@ -209,14 +209,14 @@ def cnn_subnetworks_evaluation_circle_rebuilt_cm(projection_params={"source": "a
 
 # %% Execute
 if __name__ == '__main__':
-    kernel_list = list(['origin', 'origin_gaussian', 'inverse', 'residual_mean', 'pseudoinverse'])
-    kernel_list = list(['origin', 'origin_gaussian', 'pseudoinverse'])
-    selection_rate_list = [1, 0.5, 0.3, 0.25, 0.2, 0.15, 0.1, 0.07]
-    selection_rate_list = [1, 0.5, 0.4]
+    # kernel_list = list(['origin', 'origin_gaussian', 'inverse', 'residual_mean', 'pseudoinverse'])
+    # kernel_list = list(['origin', 'origin_gaussian', 'pseudoinverse'])
+    # selection_rate_list = [1, 0.5, 0.3, 0.25, 0.2, 0.15, 0.1, 0.07]
+    
+    selection_rate_list = [0.3, 0.2, 0.1]
     
     sigma_candidates = [0.3, 0.2, 0.15, 0.1, 0.05]
-    lambda_candidates = [1e-6, 1e-5, 1e-4]
-    # lambda_candidates = [1e-3, 1e-2, 1e-1, 1]
+    lambda_candidates = [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
     
     for selection_rate in selection_rate_list:      
         # cnn_subnetworks_evaluation_circle_rebuilt_cm(projection_params={"source": "auto", "type": "3d_spherical"},
